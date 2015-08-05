5794e51bc6ae7067cf64da092c60593b0020339a
Husam Al-Ziab <husam.alziab@gmail.com>
2015-08-05 15:01:39 -0700

    Assignment 12 complete

--- BlocExercises/BlocExercises/AwesomeCounter.m
+++ BlocExercises/BlocExercises/AwesomeCounter.m
@@ -11,8 +11,86 @@
 @implementation AwesomeCounter
 
 - (NSString *) stringWithNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @"";
+
+    if (number == otherNumber) {
+        return [NSString stringWithFormat:@"%ld", (long)number];
+    }
+    
+    NSInteger lowNumber, highNumber;
+    
+    if (number > otherNumber) {
+        lowNumber = otherNumber;
+        highNumber = number;
+    } else if (otherNumber > number) {
+        lowNumber = number;
+        highNumber = otherNumber;
+    }
+    
+    NSMutableString *count = [NSMutableString string];
+    
+    for (NSInteger n = lowNumber; n <= highNumber; n++) {
+        [count appendFormat:@"%ld", (long)n ];
+    }
+    
+    return count;
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//
+//    if (number == otherNumber) {
+//        
+//       return [NSString stringWithFormat:@"%ld", (long)number];
+//    
+//    }
+//    
+//    NSInteger lowNumber, highNumber;
+//    
+//    if (otherNumber > number) {
+//    
+//        highNumber = otherNumber;
+//        lowNumber = number;
+//    
+//    } else if (number > otherNumber){
+//        highNumber = number;
+//        lowNumber = otherNumber;
+//    }
+//    
+//    NSMutableString *count = [NSMutableString string];
+//    
+//    for (NSInteger n = lowNumber; n <= highNumber; n++) {
+//        [count appendFormat:@"%ld",(long)n];
+//    }
+//    
+//    return count;
+//    
+//}
 
 @end
